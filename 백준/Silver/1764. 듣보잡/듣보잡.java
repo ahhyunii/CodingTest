2d4affc567e839a9ea3dc6c56bd34acbc655fd5a
ahhyunii <153539917+ahhyunii@users.noreply.github.com>
2026-03-22 23:06:43 +0900

    [Silver IV] Title: 듣보잡, Time: 276 ms, Memory: 26180 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -9,38 +9,27 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Map<String,Boolean> noHeardAndSaw = new HashMap<>();
+        Set<String> noHeard = new HashSet<>();
+        List<String> noHeardAndSaw = new ArrayList<>();
 
-        // 듣도 못한 사람
-        // 초기값 false로 넣기 -> 듣기만 한 사람
+        // 듣도 못한 사람 -> set에 넣기
         for(int i = 0; i < N; i++){
-            noHeardAndSaw.put(br.readLine(), Boolean.FALSE);
+            noHeard.add(br.readLine());
         }
 
         // 보도 못한 사람
-        // 듣고 봤으면 true로 넣기, 아니면 false
+        // set에 있다면 -> list에 넣기
         for(int i = 0; i < M; i++){
             String s = br.readLine();
-            if(noHeardAndSaw.containsKey(s)){
-                noHeardAndSaw.put(s, Boolean.TRUE);
-            }else{
-                noHeardAndSaw.put(s, Boolean.FALSE);
+            if(noHeard.contains(s)){
+                noHeardAndSaw.add(s);
             }
         }
 
-        // true인 사람들만 list에 추가
-        List<String> trueList = new ArrayList<>();
-        for(Map.Entry<String, Boolean> entry: noHeardAndSaw.entrySet()){
-            if(entry.getValue()){
-                trueList.add(entry.getKey());
-            }
-        }
-
-        Collections.sort(trueList); // 사전순 정렬
-        System.out.println(trueList.size());
-        for(String s: trueList){
+        Collections.sort(noHeardAndSaw); // 사전순 정렬
+        System.out.println(noHeardAndSaw.size());
+        for(String s: noHeardAndSaw){
             System.out.println(s);
         }
-
     }
 }
