f2acf657086ada282c9c86846e427308726d71c3
ahhyunii <153539917+ahhyunii@users.noreply.github.com>
2026-03-22 23:21:57 +0900

    [Silver IV] Title: 듣보잡, Time: 280 ms, Memory: 26428 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -28,8 +28,11 @@ public class Main{
 
         Collections.sort(noHeardAndSaw); // 사전순 정렬
         System.out.println(noHeardAndSaw.size());
+        
         for(String s: noHeardAndSaw){
             System.out.println(s);
         }
+        
+        br.close();
     }
 }
